b6508156c1b5a2527dbe894ed367a4c6c1073e3e
Kyle Rosenblad <kyle.c.rosenblad@gmail.com>
2024-04-06 14:02:38 -0700

    roxygenized after forgetting to do so following error message fix for buildcompnet(). updated example trait data and documentation to include categorical trait.

--- R/ex_traits.R
+++ R/ex_traits.R
@@ -3,7 +3,8 @@
 #'
 #' A data frame with 24 rows and 2 columns:
 #' \describe{
-#'   \item{ndtrait}{A trait driving competitive niche differentiation}
-#'   \item{domtrait}{A trait influencing competitive ability}
+#'   \item{ndtrait}{A continuous trait driving competitive niche differentiation}
+#'   \item{domtrait}{A continuous trait influencing competitive ability}
+#'   \item{ctrait}{A categorical trait influencing competitive niche differentiation}
 #' }
 "ex_traits"
