07d37226a1a2a7094f68511e8286e07e07d97cd5
Kyle Rosenblad <kyle.c.rosenblad@gmail.com>
2025-03-23 22:44:03 -0700

    forgot to roxygenize and rstan_config() after cutting zi mods

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -14,18 +14,14 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 
 RcppExport SEXP _rcpp_module_boot_stan_fit4ame_beta_binomial_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4ame_binomial_mod();
-RcppExport SEXP _rcpp_module_boot_stan_fit4ame_zi_binomial_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4srm_beta_binomial_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4srm_binomial_mod();
-RcppExport SEXP _rcpp_module_boot_stan_fit4srm_zi_binomial_mod();
 
 static const R_CallMethodDef CallEntries[] = {
     {"_rcpp_module_boot_stan_fit4ame_beta_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4ame_beta_binomial_mod, 0},
     {"_rcpp_module_boot_stan_fit4ame_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4ame_binomial_mod, 0},
-    {"_rcpp_module_boot_stan_fit4ame_zi_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4ame_zi_binomial_mod, 0},
     {"_rcpp_module_boot_stan_fit4srm_beta_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4srm_beta_binomial_mod, 0},
     {"_rcpp_module_boot_stan_fit4srm_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4srm_binomial_mod, 0},
-    {"_rcpp_module_boot_stan_fit4srm_zi_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4srm_zi_binomial_mod, 0},
     {NULL, NULL, 0}
 };
 
