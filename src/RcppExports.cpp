1104041fb2c3941c34c7d301620b98415634de05
Kyle Rosenblad <kyle.c.rosenblad@gmail.com>
2025-08-12 10:12:55 -0700

    added Stan files for FNCH models without OLRE

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -14,16 +14,20 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 
 RcppExport SEXP _rcpp_module_boot_stan_fit4ame_binomial_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4ame_fnchypg_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4ame_fnchypg_withoutolre_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4base_fnchypg_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4srm_binomial_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4srm_fnchypg_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4srm_fnchypg_withoutolre_mod();
 
 static const R_CallMethodDef CallEntries[] = {
     {"_rcpp_module_boot_stan_fit4ame_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4ame_binomial_mod, 0},
     {"_rcpp_module_boot_stan_fit4ame_fnchypg_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4ame_fnchypg_mod, 0},
+    {"_rcpp_module_boot_stan_fit4ame_fnchypg_withoutolre_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4ame_fnchypg_withoutolre_mod, 0},
     {"_rcpp_module_boot_stan_fit4base_fnchypg_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4base_fnchypg_mod, 0},
     {"_rcpp_module_boot_stan_fit4srm_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4srm_binomial_mod, 0},
     {"_rcpp_module_boot_stan_fit4srm_fnchypg_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4srm_fnchypg_mod, 0},
+    {"_rcpp_module_boot_stan_fit4srm_fnchypg_withoutolre_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4srm_fnchypg_withoutolre_mod, 0},
     {NULL, NULL, 0}
 };
 
